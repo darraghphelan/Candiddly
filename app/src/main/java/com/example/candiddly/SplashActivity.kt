68459f8169fab7d27d137a7e19f42b5080de7662
darraghphelan <116357743@umail.ucc.ie>
2020-02-26 23:46:48 +0000

    Splash Activity added to keep user logged in when app closes.

---
+++ app/src/main/java/com/example/candiddly/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.candiddly
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        else{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        finish()
+    }
+}
