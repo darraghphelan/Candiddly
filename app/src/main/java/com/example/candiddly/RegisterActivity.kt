8c4987827f3d591a746317780184e7cd0ae40aea
darraghphelan <116357743@umail.ucc.ie>
2020-03-10 23:18:48 +0000

    Fixed bug allowing duplicate usernames.

--- app/src/main/java/com/example/candiddly/RegisterActivity.kt
+++ app/src/main/java/com/example/candiddly/RegisterActivity.kt
@@ -8,9 +8,12 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -39,61 +42,6 @@ class RegisterActivity : AppCompatActivity() {
         loginButton = findViewById(R.id.registerLoginButton)
         registerButton = findViewById(R.id.registerRegisterButton)
 
-        registerButton.setOnClickListener{
-            val username = usernameEditText.text.toString()
-            val email = emailEditText.text.toString()
-            val password = passwordEditText.text.toString()
-
-            if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
-                Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_LONG).show()
-            } else{
-                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this)
-                { task ->
-                    if(task.isSuccessful) {
-
-                        val users = db.collection("users")
-                        val currentUser = FirebaseAuth.getInstance().currentUser
-
-                        val user = hashMapOf(
-                            "email" to email,
-                            "username" to username,
-                            "id" to currentUser?.uid.toString()
-                        )
-
-                        users.document(currentUser?.uid.toString()).set(user)
-                            .addOnSuccessListener { documentReference ->
-                                Log.d(TAG, "DocumentSnapshot added with ID: $documentReference")
-                            }
-                            .addOnFailureListener { e ->
-                                Log.w(TAG, "Error adding document", e)
-                            }
-
-                        val connections = users.document(currentUser?.uid.toString()).collection("connections")
-                        val gallery = users.document(currentUser?.uid.toString()).collection("gallery")
-
-                        val friendsData = hashMapOf("friends" to arrayListOf(""))
-                        connections.document("friends").set(friendsData)
-
-                        val receivedData = hashMapOf("received" to arrayListOf(""))
-                        connections.document("received").set(receivedData)
-
-                        val sentData = hashMapOf("sent" to arrayListOf(""))
-                        connections.document("sent").set(sentData)
-
-                        val imageUrls = hashMapOf("images" to arrayListOf(""))
-                        gallery.document("images").set(imageUrls)
-                        db.collection("users").document(currentUser?.uid.toString()).collection("gallery").document("images").update("images", FieldValue.arrayRemove(""))
-
-                        Toast.makeText(this, "Successfully Registered", Toast.LENGTH_LONG).show()
-                        val intent = Intent(this, MainActivity::class.java)
-                        startActivity(intent)
-                        finish()
-                    } else {
-                        Toast.makeText(this, "Registration Failed", Toast.LENGTH_LONG).show()
-                    }
-                }
-            }
-        }
 
         loginButton.setOnClickListener{
             val intent = Intent(this, LoginActivity::class.java)
@@ -101,5 +49,81 @@ class RegisterActivity : AppCompatActivity() {
             finish()
         }
 
+        registerButton.setOnClickListener{
+            val username = usernameEditText.text.toString()
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                toastMaker("Please fill all the fields")
+            } else {
+                lifecycleScope.launch {
+                    val docRefUsers = db.collection("users")
+                    val userDocs = docRefUsers
+                        .whereEqualTo("username", username)
+                        .get()
+                        .await()
+                    if (userDocs.isEmpty) {
+                        registration(email, password, username)
+                    } else {
+                        toastMaker("Username taken")
+                    }
+                }
+            }
+        }
+    }
+
+
+    private fun registration(email: String, password: String, username: String) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this)
+        { task ->
+            if (task.isSuccessful) {
+
+                val users = db.collection("users")
+                val currentUser = FirebaseAuth.getInstance().currentUser
+
+                val user = hashMapOf(
+                    "email" to email,
+                    "username" to username,
+                    "id" to currentUser?.uid.toString()
+                )
+
+                users.document(currentUser?.uid.toString()).set(user)
+                    .addOnSuccessListener { documentReference ->
+                        Log.d(TAG, "DocumentSnapshot added with ID: $documentReference")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w(TAG, "Error adding document", e)
+                    }
+
+                val connections =
+                    users.document(currentUser?.uid.toString()).collection("connections")
+                val gallery = users.document(currentUser?.uid.toString()).collection("gallery")
+
+                val friendsData = hashMapOf("friends" to arrayListOf(""))
+                connections.document("friends").set(friendsData)
+
+                val receivedData = hashMapOf("received" to arrayListOf(""))
+                connections.document("received").set(receivedData)
+
+                val sentData = hashMapOf("sent" to arrayListOf(""))
+                connections.document("sent").set(sentData)
+
+                val imageUrls = hashMapOf("images" to arrayListOf(""))
+                gallery.document("images").set(imageUrls)
+                db.collection("users").document(currentUser?.uid.toString()).collection("gallery").document("images").update("images", FieldValue.arrayRemove(""))
+
+                toastMaker("Successfully Registered")
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                toastMaker("Registration Failed")
+            }
+        }
+    }
+
+    private fun toastMaker(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 }
